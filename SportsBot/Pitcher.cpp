832279efe0c6eca6f029ce5ab5cbeeb921b65342
Jamieson Dunne <jamiesondunne@gmail.com>
2017-05-30 20:08:25 -0400

    Rename Project File

--- SportsBot/Pitcher.cpp
+++ SportsBot/Pitcher.cpp
@@ -5,7 +5,7 @@
 int GatePin = 9; 
 int GateSpeed = 180;
 int PitcherPinTop = 6;
-int PitcherPinBottom = ;
+int PitcherPinBottom =;
 int Rest = 90;
 int pitchSpeedTop = 180;
 int pitchSpeedBottom = 0;
